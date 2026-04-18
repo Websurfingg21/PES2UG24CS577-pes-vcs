8b9bd03adec1d136d9392aa7f4ba984d748e29c2
Vedant <vedantsharma069@gmail.com>
2026-04-18 22:30:00 +0530

    Object writing

--- commit.c
+++ commit.c
@@ -221,5 +221,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     if (commit_serialize(&commit, &data, &len) != 0)
         return -1;
 
+    ObjectID commit_id;
+    if (object_write(OBJ_COMMIT, data, len, &commit_id) != 0) {
+        free(data);
+        return -1;
+    }
+
     return 0;
 }
