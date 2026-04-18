df86438cf168d6473a9f7af082c6f403b944cc37
Vedant <vedantsharma069@gmail.com>
2026-04-18 22:29:05 +0530

    Serialize

--- commit.c
+++ commit.c
@@ -213,8 +213,13 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
     commit.timestamp = (uint64_t)time(NULL);
-    
+
     snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    void *data;
+    size_t len;
+    if (commit_serialize(&commit, &data, &len) != 0)
+        return -1;
 
     return 0;
 }
